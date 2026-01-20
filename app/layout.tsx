b85c6b9c9fb7c527af861bca934f303c6e430f2f
huijueja-tech <huijueja@gmail.com>
2026-01-20 15:11:34 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Badminton Ultimate Pro',
+  description: 'แอปจัดการก๊วนแบดมินตันอบอุ่น',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="th">
+      <body>{children}</body>
+    </html>
+  )
+}
